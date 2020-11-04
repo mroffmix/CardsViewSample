d5eff57fdc1215fa8b0ad7f5a58964768d3229fb
Ilya Mikhailov <mihailoov@gmail.com>
2020-11-04 10:05:46 +0100

    Initial Commit

---
+++ CardsViewSample/CardsViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsViewSampleApp.swift
+//  CardsViewSample
+//
+//  Created by Ilya on 04.11.20.
+//
+
+import SwiftUI
+
+@main
+struct CardsViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
